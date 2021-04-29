86cd31d0af43822cdde7ae9dc12f4f7d64426cc5
Mark Robinson <mhrobinson123@gmail.com>
2021-04-29 14:35:00 -0400

    Update init.sh

--- hope/docker/init.sh
+++ hope/docker/init.sh
@@ -3,6 +3,6 @@
 sleep 5
 
 mysql -u root -e "CREATE DATABASE hope"
-mysql -u root -e "CREATE USER dataturks@'127.0.0.1' IDENTIFIED BY '12345';"
-mysql -u root -e "GRANT SELECT, INSERT, UPDATE, DELETE ON hope.* TO dataturks@127.0.0.1;FLUSH PRIVILEGES;"
+mysql -u root -e "CREATE USER dataturks@'%' IDENTIFIED BY '12345';"
+mysql -u root -e "GRANT SELECT, INSERT, UPDATE, DELETE ON hope.* TO dataturks@'%';FLUSH PRIVILEGES;"
 mysql -u root hope < ./mysqlInit.sql
